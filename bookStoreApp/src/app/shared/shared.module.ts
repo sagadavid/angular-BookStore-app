d028627826b4d2406473c963d2f5d038d9cf082d
sagadavid <epostsaga@gmail.com>
2023-09-09 08:14:46 +0200

    toolbar import2 shared &exported4 outeruse(apcomp)

--- bookStoreApp/src/app/shared/shared.module.ts
+++ bookStoreApp/src/app/shared/shared.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-
-
-
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-  declarations: [
-    NotFoundComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [NotFoundComponent, ToolbarComponent],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
+  exports: [ToolbarComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
