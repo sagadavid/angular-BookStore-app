0f64c520f8f13832992124391f53d53553133d09
sagadavid <epostsaga@gmail.com>
2023-09-13 19:34:29 +0200

    strict initializatioin false for class

--- bookStoreApp/src/app/books/models/book.model.ts
+++ bookStoreApp/src/app/books/models/book.model.ts
@@ -1,9 +1,9 @@
 import { PriceModel } from './price.model';
 
-export interface BookModel {
-  id: number;
-  title: string;
-  totalPages: number;
-  author: string;
-  price: PriceModel;
+export class BookModel {
+  public id: number;
+  public title: string;
+  public totalPages: number;
+  public author: string;
+  public price: PriceModel;
 }
