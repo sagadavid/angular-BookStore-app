57ab8b182bf323521c264a2e6f624701f10763d9
sagadavid <epostsaga@gmail.com>
2023-09-13 16:18:31 +0200

    useFactory fra external file 2

--- bookStoreApp/src/app/shared/services/counter.factory.ts
+++ bookStoreApp/src/app/shared/services/counter.factory.ts
@@ -2,10 +2,12 @@ import { CounterService } from './counter.service';
 import { Counter2Service } from './counter2.service';
 import { TestService } from './test.service';
 
+const facts = (testService: TestService) =>
+  testService.status ? new CounterService() : new Counter2Service();
+
 export const CounterFactory = {
   //if test service is false instantiate counter2service else counterservice
   provide: CounterService,
-  useFactory: (testService: TestService) =>
-    testService.status ? new CounterService() : new Counter2Service(),
+  useFactory: facts,
   deps: [TestService], //dependancy of useFactory
 };
