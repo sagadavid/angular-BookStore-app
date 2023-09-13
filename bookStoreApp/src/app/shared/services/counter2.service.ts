07ba275ce30611859a38148f3bb1d0ad2dcb1269
sagadavid <epostsaga@gmail.com>
2023-09-13 16:49:39 +0200

    console error: missing provider added

--- bookStoreApp/src/app/shared/services/counter2.service.ts
+++ bookStoreApp/src/app/shared/services/counter2.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { CounterService } from './counter.service';
 import { DependancyService } from './dependency.service';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class Counter2Service {
   counter: number = 0;
 
