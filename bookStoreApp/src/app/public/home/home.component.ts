9a8ab7cd68e607d7e8e3a7211dfb05cc8552e36e
sagadavid <epostsaga@gmail.com>
2023-09-24 16:48:52 +0200

    code reset

--- bookStoreApp/src/app/public/home/home.component.ts
+++ bookStoreApp/src/app/public/home/home.component.ts
@@ -1,57 +1,11 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  Inject,
-  OnInit,
-  ViewChild,
-  Renderer2,
-} from '@angular/core';
-import { MatButton } from '@angular/material/button';
-import { AuthorsComponent } from 'src/app/shared/components/authors/authors.component';
-import { CounterService } from 'src/app/shared/services/counter.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit, AfterViewInit {
-  @ViewChild('authors') private childAuthor: AuthorsComponent;
-  @ViewChild('decrButton') private decrButton: MatButton;
-  @ViewChild('pageTitle') private pageTitle: ElementRef;
-
-  constructor(
-    public countrService: CounterService,
-    //inject a new service as useValue of providers
-    @Inject('appTitleToken') public titleFromToken: any,
-    private renderer: Renderer2
-  ) {}
-
-  valueInput = 'value from home.ts not home.html';
-
-  public increase(): void {
-    this.countrService.incCount();
-  }
-  public decrease(): void {
-    this.countrService.decCount();
-  }
-
+export class HomeComponent implements OnInit {
+  constructor() {}
   ngOnInit(): void {}
-
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.childAuthor.setData(234);
-      //this.decrButton.color = 'warn';
-      //this.pageTitle.nativeElement.innerHtml = 'new page title for home';
-      this.renderer.setProperty(this.decrButton, 'color', 'warn');
-      this.renderer.setProperty(
-        this.pageTitle.nativeElement,
-        'innerHTML',
-        'new page title for home'
-      );
-      console.log(this.decrButton);
-      console.log(this.pageTitle);
-    }, 0);
-  }
 }
