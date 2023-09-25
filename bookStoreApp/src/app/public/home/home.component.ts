a4e4da94f8e210557ed33b72a0084cf42f466369
sagadavid <epostsaga@gmail.com>
2023-09-25 11:04:49 +0200

    ngDoChange

--- bookStoreApp/src/app/public/home/home.component.ts
+++ bookStoreApp/src/app/public/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthorModel } from 'src/app/shared/model/author.model';
 import { TestService } from 'src/app/shared/services/test.service';
 
 @Component({
@@ -7,12 +8,20 @@ import { TestService } from 'src/app/shared/services/test.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
-  ngOnInit(): void {}
+  public forfatter: AuthorModel = { id: 22, name: 'dan brown' };
   public upCountHome: number = 0;
   public homeBool: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
   public upCounter() {
     this.upCountHome++;
     this.homeBool = !this.homeBool;
+    //if reference of object/forfatter is not changing, meaning, only value is changing, onChange cannot cath the change. check browser console. here we need ngDoCheck
+    this.forfatter.id = this.upCountHome++;
+    //an example of reference change-a new object create
+    // this.forfatter = { id: this.upCountHome++, name: 'david saga' };
   }
 }
--- bookStoreApp/src/app/shared/components/authors/authors.component.ts
+++ bookStoreApp/src/app/shared/components/authors/authors.component.ts
@@ -4,17 +4,23 @@ import {
   Input,
   OnChanges,
   SimpleChanges,
+  DoCheck,
 } from '@angular/core';
 import { TestService } from '../../services/test.service';
+import { AuthorModel } from '../../model/author.model';
 
 @Component({
   selector: 'app-authors',
   templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.css'],
 })
-export class AuthorsComponent implements OnInit, OnChanges {
+export class AuthorsComponent implements OnInit, OnChanges, DoCheck {
   constructor() {
     console.log('child constructor');
+  }
+  ngDoCheck(): void {
+    //now observe chang on browser console, get updated values by help of ngDoCheck
+    console.log(this.modelledAuthor);
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -22,6 +28,7 @@ export class AuthorsComponent implements OnInit, OnChanges {
   }
   @Input() passedData: number;
   @Input() passedBool: boolean;
+  @Input() modelledAuthor: AuthorModel;
 
   ngOnInit(): void {
     //constructor and ngOnInit runs only once, const runs first, onchanges second, oninit third(before dom)
