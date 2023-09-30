9cf0b748862a6595bacd04ca852cfea59fbf7d75
sagadavid <epostsaga@gmail.com>
2023-09-30 10:47:12 +0200

    template driven form, afterviewcheck, viewchild

--- bookStoreApp/src/app/books/components/add-book/add-book.component.ts
+++ bookStoreApp/src/app/books/components/add-book/add-book.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewChecked,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
-  styleUrls: ['./add-book.component.css']
+  styleUrls: ['./add-book.component.css'],
 })
-export class AddBookComponent {
-
+export class AddBookComponent implements AfterViewChecked {
+  @ViewChild('myNgForm') myForm: ElementRef;
+  ngAfterViewChecked(): void {
+    console.log(this.myForm);
+  }
 }
--- bookStoreApp/src/app/user/components/add-book/add-book.component.ts
+++ bookStoreApp/src/app/user/components/add-book/add-book.component.ts
@@ -1,21 +1,8 @@
-import { FormsModule } from '@angular/forms';
-import {
-  AfterViewChecked,
-  Component,
-  ElementRef,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
   styleUrls: ['./add-book.component.css'],
 })
-export class AddBookComponent implements OnInit, AfterViewChecked {
-  @ViewChild('myNgForm') myNgForm: ElementRef;
-  ngOnInit(): void {}
-  ngAfterViewChecked(): void {
-    console.log(this.myNgForm);
-  }
-}
+export class AddBookComponent {}
